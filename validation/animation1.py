6eeb468d38d4c369c11e9b7301def4dbeac6399e
Nihat <117186544+NihatSaritas@users.noreply.github.com>
2026-01-13 08:14:30 +0100

    Added comments to animation1. Implemented and commented animation2. Fixed bug with animation1 (0,0) position.

--- validation/animation1.py
+++ validation/animation1.py
@@ -3,18 +3,11 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import pandas as pd
 
-def draw_frame():
-    pass
-
-def dummy_frames():
-    frames = np.zeros((2,2,1000))
-    frames[:,:,0] = np.array([[1,0.5],[1,1.5]])
-    for i in range(1,1000):
-        frames[:,:,i] = frames[:,:,i-1] + np.array([[0.01,0.01],[0,0]])
-
-    return frames
 
 def animate(centroids, pikes, shiners):
+    """
+    Main animation loop. Saves small files as gif, big files as mp4.
+    """
     fig, ax,  = plt.subplots(figsize=(12, 6))
     ax.set_xlim((0,200))
     ax.set_ylim((0,110))
@@ -46,6 +39,10 @@ def animate(centroids, pikes, shiners):
 
 
     def update(frame_idx):
+        """
+        Update the arrows each frame with their new position and
+        orientation. Repeat for each set of arrows.
+        """
         plt.title(f'Frame {frame_idx}')
         centroid.set_offsets([[centroids[frame_idx, 0], centroids[frame_idx, 1]]])
         centroid.set_UVC(centroids[frame_idx, 2], centroids[frame_idx, 3])
@@ -65,30 +62,43 @@ def animate(centroids, pikes, shiners):
         interval=500
     )
 
-    ani.save('fish_animation.gif', writer='pillow', fps=2)
+    ani.save('fish_animation_1.gif', writer='pillow', fps=2)
     plt.show()
 
-    #plt.savefig('tmp')
 
 def read_dataset(file):
+    """
+    Read csv file input and return pandas dataframe.
+    """
     df = pd.read_csv(file)
-    #print(df.columns)
     df = df[['batch', 'id', 'x', 'y', 'hx', 'hy']]
-    #print(df.columns)
     return df
 
+
 def split_data(df):
+    """
+    Splits the dataset into positions and orientations for the fish, their centroid,
+    and the predator. This allows to easily plot each of these in a different color.
+
+    Makes assumption that the csv per frame always first contains the centroid, followed 
+    by some number of fish, and lastly the pike (before showing the centroid of the next
+    frame).
+    
+    Each frame contains a variable number of fish (shiners), and exactly 1 predator (pike).
+    """
     frame_count = len(df[df['id'] == 'cnt'])
     centroids = []
     pikes = []
-    shiners = np.zeros((frame_count, 50, 4))
+    # Larger than number of fish to serve as buffer since quiver can not handle varying 
+    # number of fish. Unused slots are put on (5000,5000), which is off the grid.
+    shiners = np.full((frame_count, 50, 4), [5000, 5000, 0, 0], dtype=np.float64)
 
     frame_counter = 0
     fish_counter = 0
     for _, row in df.iterrows():
-        if row['id'] == 'cnt':
+        if row['id'] == 'cnt':  # First row of each frame
             centroids.append(row[['x', 'y', 'hx', 'hy']].values)
-        elif row['id'] == 'PIKE':
+        elif row['id'] == 'PIKE':  # Final row of each frame
             pikes.append(row[['x', 'y', 'hx', 'hy']].values)
             frame_counter += 1
             fish_counter = 0
@@ -100,19 +110,8 @@ def split_data(df):
             
 
 def main():
-    print('main')
     df = read_dataset('dataset1/final_movedat.csv')
-    print(f'xlim ({df['x'].min()}, {df['x'].max()})')
-    print(f'ylim ({df['y'].min()}, {df['y'].max()})')
-
-    # centroids = df[df['id'] == 'cnt']
-    # pikes = df[df['id'] == 'PIKE']
-    # cts1 = centroids[centroids['batch'] == 1]
-    # pks1 = pikes[pikes['batch'] == 1]
-
     centroids, pikes, shiners = split_data(df)
-    # print(len(pks1))
-    # print(len(cts1))
     animate(centroids, pikes, shiners)
 
 
---
+++ validation/animation2.py
@@ -0,0 +1,106 @@
+""" This particular dataset does not store orientations, but we instead use the
+stored velocity in x and y direction as orientation of the fish."""
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import json as json
+
+
+NUM_FISH = 300
+
+
+def split_data(fish_dict, frame):
+    """
+    Extract data corresponding to certain framenumber, then split
+    into position and orientations.
+
+    For our animation we use the quiver method and update the arrow
+    positions and orientations. This requires a constant number of
+    fish in each frame. The number of fish tracked in each frame
+    fluctuates. This is resolved by using a 300 fish buffer for
+    the quiver, and moving untracked fish to a position off the
+    grid (5000, 5000).
+
+    Lastly, explicitly storing the data as floats in an array is
+    required to resolve an error with the animation loop internally
+    calling the function np.isfinite().
+    """
+    frame_data = fish_dict[f"{frame}"]
+    px = np.asarray(frame_data["px"], dtype=np.float64)
+    py = np.asarray(frame_data["py"], dtype=np.float64)
+    vx = np.asarray(frame_data["vx"], dtype=np.float64)
+    vy = np.asarray(frame_data["vy"], dtype=np.float64)
+
+    count = len(px)
+    if count < NUM_FISH:
+        pad_len = NUM_FISH - count
+        px = np.pad(px, (0, pad_len), constant_values=5000)
+        py = np.pad(py, (0, pad_len), constant_values=5000)
+        vx = np.pad(vx, (0, pad_len), constant_values=0)
+        vy = np.pad(vy, (0, pad_len), constant_values=0)
+
+    return px, py, vx, vy, count
+
+
+def animate(fish_dict):
+    """
+    Main animation loop. Saves small files as gif, big files as mp4.
+    """
+    # Initialize figure, set the first frame.
+    fig, ax,  = plt.subplots(figsize=(12, 6))
+    ax.set_xlim((0,2100))
+    ax.set_ylim((0,1200))
+    frame_count = len(fish_dict)
+    
+    px, py, vx, vy, count = split_data(fish_dict, 1)
+    plt.title(f'Frame 1, tracked fish: {count}')
+
+    fish = ax.quiver(px, py, vx, vy, color='orange', 
+                       angles='xy', 
+                       scale_units='xy',
+                       label='golden\nshiner',
+                    )
+
+    plt.legend(loc = 'upper right', bbox_to_anchor=(1.125, 1))
+
+
+    def update(frame_idx):
+        """
+        Update the arrows each frame with their new position and
+        orientation.
+        """
+        frame = frame_idx + 1
+        px, py, vx, vy, count = split_data(fish_dict, frame)
+        plt.title(f'Frame {frame}, tracked fish: {count}')
+
+        fish.set_offsets(np.column_stack((px, py)))
+        fish.set_UVC(vx, vy)
+
+    ani = FuncAnimation(
+        fig,
+        update,
+        frames=frame_count,
+        interval=2
+    )
+
+    # Saving as gif is very slow and large (200+ mb) for 5000 frames, 
+    # so switch to video format instead.
+    # ani.save('fish_animation_2.gif', writer='pillow', fps=60)
+    ani.save('fish_animation_2.mp4', writer='ffmpeg', fps=60)
+    plt.show()
+
+
+def read_dataset(file):
+    f = open(file)
+    file_content = f.read()
+    fish_dict = json.loads(file_content)
+    return fish_dict
+
+
+def main():
+    fish_dict = read_dataset('dataset2/schooling_frames.json')
+    animate(fish_dict)
+
+
+if __name__ == "__main__":
+    main()
